d03417808ab50d19a418f655cfe378f4cb907531
fatima-elfat <ffzelfatehy@gmail.com>
2023-11-02 23:17:41 +0100

    fix errors g

--- 110-binary_tree_is_bst.c
+++ 110-binary_tree_is_bst.c
@@ -13,14 +13,7 @@ int bt_is_bst(const binary_tree_t *tree, int min, int max)
 		return (0);
 	if (tree)
 	{
-		if (tree->left &&
-		(tree->n <= tree->left->n ||
-		tree->left->n >= max || tree->left->n <= min))
-			return (0);
-		if (tree->right &&
-		(tree->n >= tree->right->n ||
-		tree->right->n >= max || tree->right->n <= min))
-			return (0);
+		if (tree >= max || tree <= min)
 		bt_is_bst(tree->left, min, tree->n);
 		bt_is_bst(tree->right, tree->n, max);
 	}
