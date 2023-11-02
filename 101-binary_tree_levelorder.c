f5f24fda27b099342a90fa8abda3d978012c0bb7
fatima-elfat <ffzelfatehy@gmail.com>
2023-11-02 18:27:32 +0100

    fix errors a

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -19,25 +19,6 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	return ((h1 > h2) ? h1 : h2);
 }
 /**
- * binary_tree_levelorder - goes through a binary tree
- * using pre-order traversal.
- * @tree: a pointer to the root node of the tree to traverse.
- * @func: a pointer to a function to call for each node.
- * The value in the node must be passed as a parameter
- * to this function.
-*/
-void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
-{
-	size_t h, i;
-
-	if (!tree && !func)
-		return;
-	/** get height if 0 then do func*/
-	h = binary_tree_height(tree);
-	for (i = 0; i < h; i++)
-		bt_levelorder(tree, func, i);
-}
-/**
  * bt_levelorder - goes through a binary tree
  * using pre-order traversal.
  * @tree: a pointer to the root node of the tree to traverse.
@@ -59,3 +40,23 @@ void bt_levelorder(const binary_tree_t *tree, void (*func)(int), size_t level)
         bt_levelorder(tree->right, func, level);
     }
 }
+/**
+ * binary_tree_levelorder - goes through a binary tree
+ * using pre-order traversal.
+ * @tree: a pointer to the root node of the tree to traverse.
+ * @func: a pointer to a function to call for each node.
+ * The value in the node must be passed as a parameter
+ * to this function.
+*/
+void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
+{
+	size_t h, i;
+
+	if (!tree && !func)
+		return;
+	/** get height if 0 then do func*/
+	h = binary_tree_height(tree);
+	for (i = 0; i < h; i++)
+		bt_levelorder(tree, func, i);
+}
+
