db22ae4e5fbbf57bd610264a6ab468dbf21d364d
fatima-elfat <ffzelfatehy@gmail.com>
2023-11-02 19:36:26 +0100

    add task 22

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -8,5 +8,19 @@
 */
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
 {
-		
+	binary_tree_t *bt_new;
+
+	if (!tree)
+		return (NULL);
+	bt_new = malloc(sizeof(binary_tree_t));
+	if (!bt_new)
+		return (NULL);
+	bt_new->parent = tree->left;
+	bt_new->n = tree->n;
+	bt_new->left = tree->left;
+	bt_new->right = tree->right->right;
+	tree->n = tree->left->n;
+	tree->left = bt_new;
+	tree->right = tree->right->right;
+	return (bt_new);
 }
