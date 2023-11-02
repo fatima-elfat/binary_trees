b9cc095b2dc89ccb58cfbfe446668471d21924a1
fatima-elfat <ffzelfatehy@gmail.com>
2023-11-02 19:50:58 +0100

    fix errors d

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -22,5 +22,5 @@ binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
 	tree->n = tree->right->n;
 	tree->left = bt_new;
 	tree->right = tree->right->right;
-	return (tree->left);
+	return (tree);
 }
