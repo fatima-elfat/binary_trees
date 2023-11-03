f87dbdc2220a21d493a98e408aca82b9f75261be
fatima-elfat <ffzelfatehy@gmail.com>
2023-11-03 01:35:54 +0100

    add task 32

--- 122-array_to_avl.c
+++ 122-array_to_avl.c
@@ -9,5 +9,18 @@
 */
 avl_t *array_to_avl(int *array, size_t size)
 {
-		
+	size_t i, j;
+	bst_t *avl_new = NULL;
+
+	if (!array)
+		return (NULL);
+	for (i = 0; i < size; i++)
+	{
+		/**checks if val already exists*/
+		for (j = 0; j < i; j++)
+			if (array[i] == array[j])
+				break;
+		avl_insert(&avl_new, array[i]);
+	}
+	return (avl_new);
 }
