97ee63501e5279c5e16a25f873be48d3c49c3d79
fatima-elfat <ffzelfatehy@gmail.com>
2023-11-03 01:07:28 +0100

    fix Betty errors

--- 120-binary_tree_is_avl.c
+++ 120-binary_tree_is_avl.c
@@ -28,7 +28,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 int bt_is_avl(const binary_tree_t *tree, int min, int max)
 {
 	int a, b, c;
-	size_t h1 , h2;
+	size_t h1, h2;
 
 	if (tree)
 	{
